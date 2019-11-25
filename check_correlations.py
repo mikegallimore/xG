c20198a16f54a734a0db2f4e082c9fe333ea67a6
mikegallimore <35549782+mikegallimore@users.noreply.github.com>
2019-11-25 16:03:10 -0500

    Add files via upload

---
+++ check_correlations.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+@author: @mikegallimore
+"""
+###
+### IMPORT NEEDED PACKAGES
+###
+
+import pandas as pd
+import parameters
+
+###
+### LOAD & SHAPE THE DATA
+###
+
+### set the location of the file with the seed data
+shots_training_seasons = 'shots_train.csv'
+
+### create a dataframe of the seed data; duplicate it and drop unneeded columns
+shots_train_df = pd.read_csv(shots_training_seasons)
+shots_df = shots_train_df.copy()
+shots_df = shots_df.drop(columns=parameters.drop_columns)
+
+###
+### CHECK THE CORRELATIONS
+###
+
+corr = shots_df.corr()['IS_GOAL'][shots_df.corr()['IS_GOAL'] < 1].abs()
+corr = corr.sort_values(ascending=False)
+print(corr)
