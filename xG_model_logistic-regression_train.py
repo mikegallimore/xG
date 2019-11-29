756734094a6fe934e2364fb757a974a50ca209c3
mikegallimore <35549782+mikegallimore@users.noreply.github.com>
2019-11-28 23:02:39 -0500

    Update xG_model_logistic-regression_train.py

--- xG_model_logistic-regression_train.py
+++ xG_model_logistic-regression_train.py
@@ -2,9 +2,6 @@
 """
 @author: Michael
 """
-###
-### IMPORT NEEDED PACKAGES
-###
 
 import pandas as pd
 from sklearn.model_selection import GridSearchCV
@@ -17,40 +14,41 @@ import parameters
 ### LOAD & SHAPE THE DATA
 ###
 
-### set the location of the file with the seed data
+# set the location of the file with the seed data
 shots_training_seasons = 'shots_train.csv'
 
-### create a dataframe of the seed data; duplicate it and drop unneeded columns
+# create a dataframe of the seed data; duplicate it and drop unneeded columns
 shots_train_df = pd.read_csv(shots_training_seasons)
 shots_df = shots_train_df.copy()
 shots_df = shots_df.drop(columns=parameters.drop_columns)
+
 
 ###
 ### CREATE THE LOGISTIC REGRESSION
 ###
 
-### set the independent variables
+# set the independent variables
 continuous_variables = parameters.continuous_variables
 boolean_variables = parameters.boolean_variables
 independent_variables = continuous_variables + boolean_variables
 x = shots_df[independent_variables]
 
-### set the dependent variable
+# set the dependent variable
 y = shots_df['IS_GOAL']
 
-### apply the logistic regression
+# apply the logistic regression
 logreg = LogisticRegression(solver='sag', random_state=42, tol=.01, max_iter=10000)
 
-### create regularization hyperparameter space
+# create regularization hyperparameter space
 C = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
 
-### create hyperparameter options
+# create hyperparameter options
 hyperparameters = dict(C=C, penalty=['l2'])
 
-### create grid search
+# create grid search
 clf = GridSearchCV(logreg, hyperparameters, cv=10, verbose=10)
 
-### fit grid search
+# fit grid search
 clf.fit(x, y)
 
 print("\nLogistic Regression Classifier:\n", clf)
