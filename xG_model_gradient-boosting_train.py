9ab21fdc4bd5d3bd57ec24b4333d5cf5f168fbc8
mikegallimore <35549782+mikegallimore@users.noreply.github.com>
2019-11-28 23:01:57 -0500

    Update xG_model_gradient-boosting_train.py

--- xG_model_gradient-boosting_train.py
+++ xG_model_gradient-boosting_train.py
@@ -2,9 +2,6 @@
 """
 @author: @mikegallimore
 """
-###
-### IMPORT NEEDED PACKAGES
-###
 
 import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
@@ -17,10 +14,10 @@ import parameters
 ### LOAD & SHAPE THE DATA
 ###
 
-### set the location of the file with the seed data
+# set the location of the file with the seed data
 shots_training_seasons = 'shots_train.csv'
 
-### create a dataframe of the seed data; duplicate it and drop unneeded columns
+# create a dataframe of the seed data; duplicate it and drop unneeded columns
 shots_train_df = pd.read_csv(shots_training_seasons)
 shots_df = shots_train_df.copy()
 shots_df = shots_df.drop(columns=parameters.drop_columns)
@@ -30,25 +27,25 @@ shots_df = shots_df.drop(columns=parameters.drop_columns)
 ### FIT A GRADIENT BOOSTING ALGORITHM
 ###
 
-### set the independent variables
+# set the independent variables
 continuous_variables = parameters.continuous_variables
 boolean_variables = parameters.boolean_variables
 independent_variables = continuous_variables + boolean_variables
 x = shots_df[independent_variables]
 
-### set the dependent variable
+# set the dependent variable
 y = shots_df['IS_GOAL']
 
-### create the classifier
+# create the classifier
 clf = GradientBoostingClassifier(n_estimators=250, learning_rate=.1, random_state=42, verbose=10)
 
-### create hyperparameter options
+# create hyperparameter options
 param_grid = {'min_samples_split': [100, 250, 500], 'max_depth': [3]}
 
-### create grid search
+# create grid search
 cv = GridSearchCV(estimator=clf, param_grid=param_grid, cv=10)
 
-### fit grid search
+# fit grid search
 cv.fit(x, y)
 
 print("\nGradient Boosting Classifier:\n", cv)
