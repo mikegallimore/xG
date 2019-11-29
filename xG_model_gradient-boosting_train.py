f064cd43c3e96e0588f9f1db3df10430aacba7ce
mikegallimore <35549782+mikegallimore@users.noreply.github.com>
2019-11-28 22:48:03 -0500

    Add files via upload

---
+++ xG_model_gradient-boosting_train.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+@author: @mikegallimore
+"""
+###
+### IMPORT NEEDED PACKAGES
+###
+
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
+import pickle
+import parameters
+
+
+###
+### LOAD & SHAPE THE DATA
+###
+
+### set the location of the file with the seed data
+shots_training_seasons = 'shots_train.csv'
+
+### create a dataframe of the seed data; duplicate it and drop unneeded columns
+shots_train_df = pd.read_csv(shots_training_seasons)
+shots_df = shots_train_df.copy()
+shots_df = shots_df.drop(columns=parameters.drop_columns)
+
+
+###
+### FIT A GRADIENT BOOSTING ALGORITHM
+###
+
+### set the independent variables
+continuous_variables = parameters.continuous_variables
+boolean_variables = parameters.boolean_variables
+independent_variables = continuous_variables + boolean_variables
+x = shots_df[independent_variables]
+
+### set the dependent variable
+y = shots_df['IS_GOAL']
+
+### create the classifier
+clf = GradientBoostingClassifier(n_estimators=250, learning_rate=.1, random_state=42, verbose=10)
+
+### create hyperparameter options
+param_grid = {'min_samples_split': [100, 250, 500], 'max_depth': [3]}
+
+### create grid search
+cv = GridSearchCV(estimator=clf, param_grid=param_grid, cv=10)
+
+### fit grid search
+cv.fit(x, y)
+
+print("\nGradient Boosting Classifier:\n", cv)
+
+# save model
+pickle.dump(cv, open("pickle_gb.pkl", 'wb'))
---
+++ xG_model_logistic-regression_test.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+@author: @mikegallimore
+"""
+###
+### IMPORT NEEDED PACKAGES
+###
+
+import pandas as pd
+import pickle
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix, roc_curve, auc, log_loss, roc_auc_score
+import seaborn as sn
+import matplotlib.pyplot as plt
+import numpy as np
+import parameters
+
+###
+### LOAD & SHAPE THE DATA
+###
+
+### set the location of the file with the seed data
+shots_test_season = 'shots_test.csv'
+
+### create a dataframe for the seed and test data; duplicate it and drop unneeded columns
+test_shots_df = pd.read_csv(shots_test_season)
+shots_df = test_shots_df.copy()
+shots_df = shots_df.drop(columns=parameters.drop_columns)
+
+###
+### CREATE THE LOGISTIC REGRESSION
+###
+
+### set the independent variables for the training (5 seasons) and test (1 season) sets
+continuous_variables = parameters.continuous_variables
+boolean_variables = parameters.boolean_variables
+independent_variables = continuous_variables + boolean_variables
+x = shots_df[independent_variables]
+
+### set the dependent variable
+y = shots_df['IS_GOAL']
+
+pkl_logreg = "pickle_logreg.pkl"
+with open(pkl_logreg, 'rb') as file_logreg:
+    model_logreg = pickle.load(file_logreg)
+
+score_logreg = model_logreg.score(x, y)
+print("Test score: {0:.2f} %".format(100 * score_logreg))
+
+y_pred = model_logreg.predict(x)
+print('Accuracy: ', metrics.accuracy_score(y, y_pred))
+
+c_matrix = confusion_matrix(y_pred, y)
+print(confusion_matrix(y_pred, y))
+c_matrix_2 = pd.crosstab(y, y_pred, rownames=['Actual'], colnames=['Predicted'])
+sn.heatmap(c_matrix_2, annot=True)
+
+### generate the ROC curve
+fpr, tpr,_=roc_curve(y_pred, y, drop_intermediate=False)
+
+plt.figure()
+##Adding the ROC
+plt.plot(fpr, tpr, color='red',
+ lw=2, label='ROC curve')
+##Random FPR and TPR
+plt.plot([0, 1], [0, 1], color='blue', lw=2, linestyle='--')
+##Title and label
+plt.xlabel('FPR')
+plt.ylabel('TPR')
+plt.title('ROC curve')
+plt.show()
+
+### generate the AUC score
+auc_rating = roc_auc_score(y_pred, y)
+print('AUC Score: ', auc_rating)
+
+print('Log Loss:\n', metrics.log_loss(y, y_pred))
+
+print('Classification Report:\n', metrics.classification_report(y, y_pred))
+
+predictions = model_logreg.predict_proba(x)
+print(predictions)
+
+predictions_df = pd.DataFrame(predictions)
+
+predictions_df.to_csv('test_predictions_logreg.csv', index=False)
